0764961ad461c920b1b01849d1571edf2861f0f6
axelzzz <axelzavier@gmail.com>
2020-03-06 23:33:41 +0100

    get boooks metadata ok

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../models/Book';
 import { SearchEngineService } from '../services/search-engine.service';
+import { Observable } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
+import { Test } from '../models/Test';
 
 @Component({
   selector: 'app-main',
@@ -10,27 +13,48 @@ import { SearchEngineService } from '../services/search-engine.service';
 export class MainComponent implements OnInit {
 
   books: Book[];
+  nbLivres = 5;
 
   constructor(private searchEngineService: SearchEngineService) { }
 
   ngOnInit(): void {
     this.getBooks();
+    
   }
 
-  getBooks(): void {
-    this.searchEngineService.getBooks();
+  
+  getBooks() {
+    this.searchEngineService.getBooks()
+    .subscribe(books => {
+      this.books = books;
+    });
+  }
+
+  getAPI() {
+    this.searchEngineService.getAPI()
+    .subscribe(response => {
+      this.nbLivres = response;
+      console.log('ici ',this.nbLivres);
+    }, (error) => {
+      console.log('error',error)
+    });
   }
 
   getBooksSearched(pattern: string) {
     this.searchEngineService.getBooksSearched(pattern);
   }
-  /*
-  getBooks(): void {
-    this.searchEngineService.getBooks()
-    .subscribe(books => (this.books = books));
-  }
-*/
 
+  fetch() {
+    this.fetchBook('http://www.gutenberg.org/files/21/21-0.txt');
+  }
+
+  fetchBook(url: string) {
+    this.searchEngineService.fetchBook(url)
+    .subscribe(response => {
+      console.log('ici ', response.text);
+    })
+  }
+ 
 
 
 }
--- src/app/services/search-engine.service.ts
+++ src/app/services/search-engine.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { configUrl, httpOptions, httpOptionsCORS } from "../../assets/config";
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class SearchEngineService {
 
-  configUrl = 'assets/config.json';
+
+export class SearchEngineService {
 
   constructor(private http: HttpClient) { }
 
-  getBooks() {
-    console.log('aaaaa');
+  getBooks() : Observable<any> {
+    return this.http.get(configUrl.searchUrl);
   }
 
   getBooksSearched(pattern: string) {
@@ -20,5 +23,14 @@ export class SearchEngineService {
 
   getBooksSearchedV2() {
     //return this.http.get<>(this.configUrl);
+  }
+
+  getAPI(): Observable<any>{
+    return this.http.get<any>(configUrl.proxyUrl + configUrl.searchUrl);
+  }
+
+  fetchBook(url: string):Observable<any> {
+    console.log('iciiii')
+    return this.http.get(configUrl.proxyUrl + url);
   }
 }
