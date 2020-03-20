b2fc0a0702483a38bad6ec9b7aa0c108730f5ef0
axelzzz <axelzavier@gmail.com>
2020-03-20 22:27:39 +0100

    deploy front ok

--- server.js
+++ server.js
@@ -7,9 +7,5 @@ app.use(express.static(__dirname + '/dist'));
 app.get('/*', (req, res) => {
 	res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
-/*
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/bibli-search-frontend/index.html'));
-})
-*/
+
 app.listen(process.env.PORT || 8080);
