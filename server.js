02a0d5d9490cc5e0dbf0d777499b4e6873f1e6a2
axelzzz <axelzavier@gmail.com>
2020-03-20 19:13:39 +0100

    debug for horuko initial commit

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static( __dirname + '/front-end/dist/'));
+app.use(express.static('${__dirname}/front-end/dist/)');
+//app.use(express.static( __dirname + '/front-end/dist/'));
 app.listen(process.env.PORT || 8080);
 app.get('*', (request, response) => {
-	response.sendFile('./front-end/dist/index.html');
+	response.sendFile(`./front-end/dist/index.html`);
 });
 /*
 app.get('/*', function(req, res) {
