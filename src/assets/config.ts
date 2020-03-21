dc47c668cb859afeebc130443ccda6a5cf5c6849
axelzzz <axelzavier@gmail.com>
2020-03-21 22:12:46 +0100

    change url for backend

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,8 +1,8 @@
 export const configUrls = {
     "searchUrlLocal": "http://127.0.0.1:8000/bibliSearch/" ,
     "filterUrlLocal": "http://127.0.0.1:8000/bibliSearch/filter?pattern=" ,
-    "searchUrl": "https://xxl-library-sengine.herokuapp.com/bibliSearch/",
-    "filterUrl": "https://xxl-library-sengine.herokuapp.com/bibliSearch/filter?pattern=",
+    "searchUrl": "https://cloud-project-backend-django.herokuapp.com/bibliSearch/",
+    "filterUrl": "https://cloud-project-backend-django.herokuapp.com/bibliSearch/filter?pattern=",
     "gutenbergUrl" : "www.gutenberg.org/cache/epub/",
     "gutenbergUrl2" : "https://www.gutenberg.org/files/",
     "proxyUrl":"https://cors-anywhere.herokuapp.com/"  
