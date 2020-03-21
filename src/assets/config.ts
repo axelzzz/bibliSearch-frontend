32d7f4a0ceb0a1b36eeb66c76bfafcb6b210cbd3
axelzzz <axelzavier@gmail.com>
2020-03-21 15:56:53 +0100

    config for heroku backend

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,6 +1,8 @@
 export const configUrls = {
-    "searchUrl": "http://127.0.0.1:8000/bibliSearch/" ,
-    "filterUrl": "http://127.0.0.1:8000/bibliSearch/filter?pattern=" ,
+    "searchUrlLocal": "http://127.0.0.1:8000/bibliSearch/" ,
+    "filterUrlLocal": "http://127.0.0.1:8000/bibliSearch/filter?pattern=" ,
+    "searchUrl": "https://xxl-library-sengine.herokuapp.com/bibliSearch/",
+    "filterUrl": "https://xxl-library-sengine.herokuapp.com/bibliSearch/filter?pattern=",
     "gutenbergUrl" : "www.gutenberg.org/cache/epub/",
     "gutenbergUrl2" : "https://www.gutenberg.org/files/",
     "proxyUrl":"https://cors-anywhere.herokuapp.com/"  
