07e3b9a58d20398023d8e362d8240d74ebd2dbe5
axelzzz <axelzavier@gmail.com>
2020-03-29 12:07:50 +0200

    modify pipe

--- src/app/book/pattern.pipe.ts
+++ src/app/book/pattern.pipe.ts
@@ -4,13 +4,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PatternPipe implements PipeTransform {
     transform(text: string, pattern: string) {
         if(pattern !== "") {            
-            var result = text.replace(new RegExp(pattern),
+            var result = text.replace(new RegExp(pattern, 'g'),
             `<span class="red">${pattern}</span>`);
 
-            result = result.replace(new RegExp(pattern.charAt(0).toUpperCase() + pattern.slice(1)),
+            result = result.replace(new RegExp(pattern.charAt(0).toUpperCase() + pattern.slice(1), 'g'),
             `<span class="red">${pattern.charAt(0).toUpperCase() + pattern.slice(1)}</span>`);
 
-            return result.replace(new RegExp(pattern.toUpperCase()),
+            return result.replace(new RegExp(pattern.toUpperCase(), 'g'),
             `<span class="red">${pattern.toUpperCase()}</span>`);
         }
         else{
