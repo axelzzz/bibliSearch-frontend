0f1cc37f4a4c44c0785aad79e255b03fa2f3789e
axelzzz <axelzavier@gmail.com>
2020-03-29 11:58:21 +0200

    modify pipe

--- src/app/book/pattern.pipe.ts
+++ src/app/book/pattern.pipe.ts
@@ -7,6 +7,9 @@ export class PatternPipe implements PipeTransform {
             var result = text.replace(new RegExp(pattern),
             `<span class="red">${pattern}</span>`);
 
+            result = result.replace(new RegExp(pattern.charAt(0).toUpperCase() + pattern.slice(1)),
+            `<span class="red">${pattern.charAt(0).toUpperCase() + pattern.slice(1)}</span>`);
+
             return result.replace(new RegExp(pattern.toUpperCase()),
             `<span class="red">${pattern.toUpperCase()}</span>`);
         }
